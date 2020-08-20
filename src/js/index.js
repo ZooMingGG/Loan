7f3d6fed878714edf21dc5be4439b7c0d9e265ea
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2020-08-20 13:56:19 +0300

    did main videoplayer

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 'use strict';
-import Slider from './modules/slider';
+import Slider from './modules/Slider';
+import VideoPlayer from './modules/VideoPlayer';
+
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
-
     slider.render();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 });
